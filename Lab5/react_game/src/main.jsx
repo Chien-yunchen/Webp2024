e456b8144d5ac602faf595978142a41726fbb1cf
Chien-yunchen <120553375+Chien-yunchen@users.noreply.github.com>
2024-05-08 16:17:59 +0800

    Add files via upload

---
+++ Lab5/react_game/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Geactgame from './reactgame.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Geactgame />
+  </React.StrictMode>,
+)
